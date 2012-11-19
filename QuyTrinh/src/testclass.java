5171189ca6efceaf6ad20522ac74972116761b8e
azurevn <azurevn@hotmail.com>
2012-11-19 18:48:02 +0700

    tạo cái testclass để thử github trên máy azurevn

---
+++ QuyTrinh/src/testclass.java
@@ -0,0 +1,23 @@
+
+public class testclass {
+	int a = 0;
+	int b = 0;
+	public testclass(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	public int getA() {
+		return a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+
+}
